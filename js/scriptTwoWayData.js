d0838bf7e5cd7593796887ba9fb3dc190a75a46e
Shailesh Rudra <rudra12shailesh@gmail.com>
2016-07-10 20:04:28 +0530

    Two way data binding

---
+++ js/scriptTwoWayData.js
@@ -0,0 +1,14 @@
+/**
+ * Created by shailesh on 10/7/16.
+ */
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var employee = {
+            firstName: "SHAILESH",
+            lastName: "RUDRA",
+            gender: "MALE"
+        };
+        $scope.employee = employee;
+    });
+
